24c455d98f6162d86274c0fd2e03946b69872246
blaise <blaise@advancedkiosks.com>
2019-12-05 13:51:05 -0500

    changed filepath

--- src/globalPlugins/nodeIPC.py
+++ src/globalPlugins/nodeIPC.py
@@ -3,8 +3,9 @@ import os
 
 def letZamokKnow(message):
     filename = "ZamokNVDA.dat"
-    directory = os.getenv('LOCALAPPDATA')
-    path = directory + "\\" + filename
+    directory = os.getenv(
+        'LOCALAPPDATA') + "\\zamok-browser-shell\\User Data\\Default\\Zamok\\"
+    path = directory + filename
     file_object = open(path, "w")
     file_object.write(message)
     file_object.close()
