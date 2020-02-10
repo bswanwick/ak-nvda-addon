f185645c0a6e9bec1f6579cae00d8e802f493df3
blaise <blaise@advancedkiosks.com>
2020-02-10 15:47:31 -0500

    added gestures and functionality for controlling speech rate and announcing numerical key presses

--- src/globalPlugins/init.py
+++ src/globalPlugins/init.py
@@ -28,10 +28,48 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
             msg.message("Hello. Advanced kiosk speech activated.")
             nodeIPC.letZamokKnow("true")
 
+    def script_sayStuff(self, gesture): 
+        curSpeechMode = speech.speechMode
+        
+        if curSpeechMode == speech.speechMode_talk:
+            if gesture.displayName == "alt+shift+8":
+                starDigit = "*"
+                starMsg = starDigit + " key selected."
+                msg.message(starMsg)
+                print(starMsg)
+
+            elif gesture.displayName == "alt+shift+3":
+                poundDigit = "#"
+                poundMsg = poundDigit + " key selected."
+                msg.message(poundMsg)
+                print(poundMsg)
+
+            else:
+                digit = gesture.displayName[-1]
+                numMsg = "Number " + digit + " selected."
+                msg.message(numMsg)
+                print(numMsg)
+
+    def script_setDefaultRate(self, gesture):
+        speech.getSynth()._set_rate(50)
+
     def terminate(self):
         nodeIPC.letZamokKnow("false")
 
     __gestures = {
         "kb:NVDA+S": "doStuff",
         "kb:ALT+S": "doStuff",
+        "kb:ALT+0": "sayStuff",
+        "kb:ALT+1": "sayStuff",
+        "kb:ALT+2": "sayStuff",
+        "kb:ALT+3": "sayStuff",
+        "kb:ALT+4": "sayStuff",
+        "kb:ALT+5": "sayStuff",
+        "kb:ALT+6": "sayStuff",
+        "kb:ALT+7": "sayStuff",
+        "kb:ALT+8": "sayStuff",
+        "kb:ALT+9": "sayStuff",
+        "kb:ALT+SHIFT+8": "sayStuff",
+        "kb:ALT+SHIFT+3": "sayStuff",
+        "kb:ALT+J": "setDefaultRate",
     }
